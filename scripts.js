b077699a3c5f5047ad1dc451a0123be8dfdc7232
P SATHISH KUMAR <sathishp.railway@gmail.com>
2018-02-26 23:26:05 +0530

    not much should edit more

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// get all the elements
+
+const player = document.querySelector('.player')
+const viewer = document.querySelector('.viewer')
+const progress = document.querySelector('.progress')
+
+//functions for the elements
+
+console.log(viewer)
