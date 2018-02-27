3e6ffc5f776f4187fb0766df102d815893fb47d5
P SATHISH KUMAR <sathishp.railway@gmail.com>
2018-02-27 17:17:48 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// get all the elements
-
-const player = document.querySelector('.player')
-const viewer = document.querySelector('.viewer')
-const progress = document.querySelector('.progress')
-
-//functions for the elements
-
-console.log(viewer)
